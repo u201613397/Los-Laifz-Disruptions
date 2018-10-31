babc497408acd99c084afda9db53ebcc9422e506
Unknown <niltonmc96@gmail.com>
2018-10-31 15:31:49 -0500

    Example Commit
    - Project base advances.

---
+++ Takos Quest/Assets/LevelButtonControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelButtonControl : MonoBehaviour {
+
+	public int levelValue;
+	public Text levelText;
+	// Use this for initialization
+	void Start () {
+		SetInitialValues ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetInitialValues(){
+		string tmp = "";
+		if (levelValue < 10) {
+			tmp = "0";
+		}
+		levelText.text = tmp + levelValue.ToString ();
+	}
+}
